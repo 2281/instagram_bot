fe1512c834231d0d95ea0e55b553a90d21fcd4e5
2281 <askarbin@rambler.ru>
2021-02-17 21:35:32 +0300

    Initial commit

---
+++ instabot_publicator.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3.5
+# -*- coding: utf-8 -*-
+
+'''
+my JSON config struct by default:
+{\n
+"username":"my_insta_name",\n
+"password":"my_insta_pass",\n
+"dir":"photo_for_publications"\n
+"timeout":"10"\n
+}
+'''
+
+'''
+when bot see that in photo_for_publications you put a new photo.jpg,
+he publicate it on your insta page
+and replase photo to folder photo_for_publications/publicated/
+wait timeout and see in photo_for_publications again
+'''
+
+import os
+from instabot import Bot
+import instabot
+from time import sleep
+import json
+
+if not os.path.exists("config.json"):
+	os.mknode("config.json")
+
+
+with open("config.json", "r", encoding="utf-8") as f:
+	config = json.load(f)
+
+dir = config["dir"]
+timeout = config["timeout"] * 60
+
+if not os.path.exists(dir): 
+	os.mkdir(dir)
+	os.mkdir("{}/publicated".format(dir))
+	print("{}/publicated".format(dir))
+
+bot = Bot()
+bot.login(username = config["username"], password = config["password"])
+photos = os.listdir(dir)
+
+def publication(photo):
+	bot.upload_photo(photo, caption = "")
+
+while True:
+	for pub_file in os.listdir(dir):
+		#if os.path.isfile(pub_file): contunue
+		if "jpg" or "JPG" in pub_file and not "REMOVE_ME" in pub_file and os.path.isfile(pub_file):
+			publication("{}/{}".format(dir, pub_file))
+			print("Publication: {}".format(pub_file))
+			sleep(timeout)
+			try:
+				os.replace("{}/{}.REMOVE_ME".format(dir, pub_file), "{}/{}/{}".format(dir,"publicated", pub_file))
+			except: continue
+			sleep(timeout)
